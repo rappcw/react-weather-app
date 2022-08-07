dbc433481bcdc01ae9b4d65835c2c307747cdfe2
rappcw <100694815+rappcw@users.noreply.github.com>
2022-08-07 19:14:21 +0800

    Tidy up structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  let weatherData = {
+    city: "London",
+    temperature: "20 ",
+    date: "9 July 2022",
+    description: "Sunny",
+    rain: "6",
+    uv: "3 ",
+    wind: "6",
+    min: " 7",
+    max: " 27",
+    sunrise: "05:40",
+    sunset: "17:54",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="Search">
+      <div className="container-fluid outline">
+        <form>
+          <div className="search-row">
+            <input
+              type="search"
+              className="search-form"
+              placeholder="Enter city name"
+              autoComplete="off"
+              autoFocus="on"
+            />
+            <button className="button button-hover button-blue">
+              <i className="fa-solid fa-magnifying-glass" />
+            </button>
+            <button className="button button-hover button-grey">
+              <i className="fa-solid fa-magnifying-glass-location" />
+            </button>
+          </div>
+        </form>
+
+        <h1>{weatherData.city}</h1>
+        <h2>{weatherData.date}</h2>
+        <div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt=""
+          />
+          <span className="current-temp">{weatherData.temperature}</span>
+          <span className="current-celsius">°C</span>
+
+          <div className="column">
+            <span>
+              Rain:
+              <span className="bold">{weatherData.rain}</span> ml
+            </span>
+            <span>
+              UV:
+              <span className="bold">{weatherData.uv}</span>
+              <i className="fa-solid fa-circle" />
+            </span>
+            <span>
+              Wind:
+              <span className="bold">{weatherData.wind}</span>
+              m/s
+            </span>
+          </div>
+
+          <div className="mt-3">
+            <span className="description bold">{weatherData.description}</span>
+            <span className="weather-forecast-temperature">
+              {weatherData.min}°min
+            </span>
+            <span className="weather-forecast-temperature">
+              {weatherData.max}° max
+            </span>
+            <span>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                className="suntime"
+                alt="sunrise"
+              />
+              {weatherData.sunrise}
+            </span>
+            <span>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                className="suntime"
+                alt="sunset"
+              />
+              {weatherData.sunset}
+            </span>
+          </div>
+          <div className="forecast-row"></div>
+        </div>
+      </div>
+      <div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/rappcw/react-weather-app"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Open Source code by Charmaine Rapp
         </a>
-      </header>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import Weather from "./Weather";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
