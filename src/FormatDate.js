abebb426e3a02a112ff5937101c396532b6b0613
rappcw <100694815+rappcw@users.noreply.github.com>
2022-08-10 21:32:17 +0800

    Update FormatDate.js

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 export default function FormatDate(props) {
-  /*let day = props.date.getDay();*/
   let hours = props.date.getHours();
-  let minutes = props.date.getMinutes();
   if (hours < 10) {
-    hours = `0${minutes}`;
+    hours = `0${hours}`;
   }
 
+  let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
